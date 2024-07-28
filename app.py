ac89eebdaee5f8ed884db33b776b7076f98e0f66
Ramazan Berk <ramazanberksirin@gmail.com>
2024-07-28 20:59:44 +0300

    pre-bugfix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pyautogui
-import os
+import subprocess
 from time import sleep
 
 class App():
@@ -8,44 +8,47 @@ class App():
         self.download_start = "download_start.png"
         pyautogui.FAILSAFE = False
 
+
     def clickDownloadButton(self) -> None:
         download_button_location = pyautogui.locateCenterOnScreen(self.download_button, confidence=0.85)
         pyautogui.click(download_button_location)
 
         print("Download button clicked")
+
     
     def isProcessRunning(self, process_name: str) -> bool:
         try:
-            os.system(f"tasklist /FI \"IMAGENAME eq {process_name}\" | findstr {process_name}")
+            subprocess.check_output(f'tasklist /FI "IMAGENAME eq {process_name}"', shell=True)
             return True
         except:
             return False
+        
 
     def waitUntilEdgeIsClosed(self) -> None:
         PROCNAME = "msedge.exe"
 
-        # Wait until download started, then close the app
         while True:
             try:
                 pyautogui.locateCenterOnScreen(self.download_start, confidence=0.85)
                 print("Download started...")
-
-                sleep(.5)
                 
-                os.system(f"taskkill /f /im {PROCNAME}")                
+                subprocess.run("taskkill /f /im msedge.exe", shell=True)
+                subprocess.run("taskkill /f /im msedgewebview2.exe", shell=True)
+                sleep(1)
                 break
 
             except pyautogui.ImageNotFoundException:
                 print("Download hasn't started yet...")
 
-                # Check if the process is running or hasn't started yet, if not, return
                 if not self.isProcessRunning(PROCNAME):
                     print("Edge is not running...")
                     return
+                
         
     def run(self) -> None:
         try:
             self.clickDownloadButton()
+            sleep(.5)
             self.waitUntilEdgeIsClosed()
 
         except pyautogui.ImageNotFoundException:
